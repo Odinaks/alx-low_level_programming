4f21a912b06f85b30bf3faa89a6be7390e97b226
Iwuala Odinakachi <odinakachiiwuala@gmail.com>
2022-03-15 12:39:08 -0700

    main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * main - entry point
- * Decription: program that prints putchar
- * followed by a new line
+ * Decription: program that prints _putchar,
+ * followed by a new line.
  * Return: 0;
  */
-int main(void)
+int _putchar(char c)
 {
 	_putchar('_');
 	_putchar('p');
