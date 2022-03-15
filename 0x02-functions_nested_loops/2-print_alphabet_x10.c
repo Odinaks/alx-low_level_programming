d14c5b9aa6f987a61b0bfcbbfd8a4386761d5f02
Iwuala Odinakachi <odinakachiiwuala@gmail.com>
2022-03-15 16:46:21 -0700

    alpha10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_10x - function
+ * Description - function that prints ten times the alphabet,
+ * followed by a new line.
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+	
+		for (i = 0; i < 10; i++)
+		{
+			for (j ='a'; j <= 'z'; j++)
+			{
+				_putchar(j)
+			}
+				_putchar('\n')
+		}
+	
+}
