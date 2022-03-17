cec33a8017679e2a0963bb8676d1c44b6e67d6e0
Iwuala Odinakachi <odinakachiiwuala@gmail.com>
2022-03-17 08:32:35 -0700

    nestedloops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
+ * _isupper - function that checks for uppercase charaters
+ * @c: character to check
  * Return: Always 0 or 1
  */
-int _isdigit(int c);
+
+int _isupper(int c);
 {
 	return(c >= 65 && c <= 90)
 }
--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _isdigit - function that checks for a digit 
+ * @c: character to check
+ * Return: Always 0 or 1
  */
 int _isdigit(int c);
 {
-
+	return(c >= 48 && c <= 57);
 }
