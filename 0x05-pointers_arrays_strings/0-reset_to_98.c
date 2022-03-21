bb8ef6360e6814fc9bebc47ef2fee1ff6a1647b9
Iwuala Odinakachi <odinakachiiwuala@gmail.com>
2022-03-21 09:16:32 -0700

    resetto98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * void reset_to_98 - function that takes a pointer to an int
+ * reset_to_98 - function that takes a pointer to an int
  * @n: pointer to int
  * Return: Always 0.
  */
