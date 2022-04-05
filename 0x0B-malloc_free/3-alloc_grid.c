0cea3acdde488730a9b4a752c5345a711f6befdb
Iwuala Odinakachi <odinakachiiwuala@gmail.com>
2022-04-05 13:15:00 -0700

    returnsAPointer

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,8 @@ int **alloc_grid(int width, int height)
 	grid = (int **)malloc(height * sizeof(int *));
 	if (grid == NULL)
 		return (NULL);
-	for (i = 0; i < height; i++)														{
+	for (i = 0; i < height; i++)
+	{
 		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
