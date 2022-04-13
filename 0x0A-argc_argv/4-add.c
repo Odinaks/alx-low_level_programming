91284b17895f3782556b0f7da08b3db37c6b9491
Iwuala Odinakachi <odinakachiiwuala@gmail.com>
2022-04-13 14:21:48 -0700

    change

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return; Always 0 (Success)
+ */
+
+int check_num(char *str)
+{}
