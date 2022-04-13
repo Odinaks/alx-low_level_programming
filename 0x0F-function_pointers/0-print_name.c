aeb67e09e23a5f82ba7681080bb8c51744576976
Iwuala Odinakachi <odinakachiiwuala@gmail.com>
2022-04-13 11:15:21 -0700

    printsAName

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: The name to print
+ * @f: Function that prints the name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
