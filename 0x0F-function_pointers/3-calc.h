78b78354853897fab74159cab386b3941feb607b
Iwuala Odinakachi <odinakachiiwuala@gmail.com>
2022-04-13 13:22:22 -0700

    performSimpleoperations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
-#ifndef CALC_H
-#define CALC_H
+#ifndef _CALC_H_
+#define _CALC_H_
 
 /**
  * struct op - Struct op
@@ -19,7 +19,6 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-
 int (*get_op_func(char *s))(int, int);
 
 #endif
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,5 @@
 #include "3-calc.h"
-#include <stdlib.h>
+#include <string.h>
 
 /**
  * get_op_func - get the function for a operator
